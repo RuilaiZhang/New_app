00c5329278edbfec330a88e1a58860d60b6a487e
Ruilai Zhang <ray.zhangruilai@gmail.com>
2021-08-11 14:23:23 +1000

    merge

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,7 @@
 module ListingsHelper
+    def image_select(listing)  
+        return listing.picture if listing.picture.attached?
+        #if no picture attached use default pic
+        return "default_image.jpg"
+    end
 end
