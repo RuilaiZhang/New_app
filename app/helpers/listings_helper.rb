d10669e92a86251a5d4f23603b74311320669f4c
Ruilai Zhang <ray.zhangruilai@gmail.com>
2021-08-12 02:45:56 +1000

    new

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -2,6 +2,6 @@ module ListingsHelper
     def image_select(listing)  
         return listing.picture if listing.picture.attached?
         #if no picture attached use default pic
-        return "default_image.jpg"
+        return "/assets/default_image.jpg"
     end
 end
